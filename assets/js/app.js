ad06678f1db7b19c2fd8dd99b97275b2ded4f04a
IA-Díaz-7 <iber.diaz.go@gmail.com>
2023-07-03 17:54:34 -0600

    ejercicio mas calificacion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
+//IBER ALEJANDRO PEREZ DIAZ CAL:4
+
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.getElementsByClassName('name');
+const $b = document.getElementsByClassName('blog');
+const $l = document.getElementsByClassName('location');
 
-function displayUser(username) {
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
@@ -15,8 +17,8 @@ function displayUser(username) {
 
 function handleError(err) {
   console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  console.log(`Algo salió mal, ${err}`);
+  //n.textContent =
 }
 
 displayUser('stolinski').catch(handleError);
